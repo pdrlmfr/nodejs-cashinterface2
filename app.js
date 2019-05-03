53974ec75856d56e1a935bd0d00b9102cb7671f3
Pedro Faria <44289144+pdrlmfr@users.noreply.github.com>
2019-05-03 12:22:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const SerialPort = require("serialport");
-
-const portname = "COM1";
-
 const Readline = require("@serialport/parser-readline");
-const port = new SerialPort(portname, {
-  baudRate: 19200
+
+let MYport;
+
+SerialPort.list((err, ports) => {
+  ports.forEach(port => {
+    console.log(port);
+    if (port.pnpId == "ACPI\\PNP0501\\0") {
+      console.log("Found It");
+      MYport = port.comName.toString();
+      console.log(MYport);
+    }
+  });
+
+  const port = new SerialPort(MYport, {
+    baudRate: 19200,
+    autoOpen: false
+  });
+
+  const parser = new Readline();
+  port.pipe(parser);
+  parser.on("data", line => console.log(`line> ${line}`));
+  parser.on("error", error => console.log(`error> ${error}`));
+
+  port.open(error => {
+    console.log("OPEN");
+    console.log(error);
+    port.write("CASH_TOTALBLOCKING 0\r\n");
+  });
 });
-
-const parser = new Readline();
-port.pipe(parser);
-
-parser.on("data", line => console.log(`line> ${line}`));
-parser.on("error", error => console.log(`error> ${error}`));
-
-port.write("CASH_TOTALBLOCKING 0");
-console.log("--END--");
